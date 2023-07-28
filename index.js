a9c016ebfe39f061f040a54fcdbea578e7844ddf
AnasOnGit <50570153+AnasOnGit@users.noreply.github.com>
2023-07-29 02:42:43 +0300

    Add support for new URL's in YouTube

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ const possibleDomains = [
   "m.youtube.com/",
   "https://youtube.com/shorts/I1I1i1i1I1I",
 ];
+// possible video links on youtube
+const possibleVideoLinks = [
+  "youtu.be",
+  "https://www.youtube.com/shorts/",
+  "https://youtube.com/shorts/",
+  "http://m.youtube.com/shorts/",
+  "https://m.youtube.com/shorts/",
+  "youtube.com/watch?v=",
+  "m.youtube.com/watch?v=",
+  "https://www.youtube.com/embed/",
+  "https://www.youtube.com/watch/",
+  "https://www.youtube.com/live/",
+];
+
+// possbile channel link format on youtube
+const possibleChannelLinks = [
+  ".youtube.com/@",
+  ".youtube.com/channel/",
+  ".youtube.com/c/",
+  ".youtube.com/user/",
+]
+
 // check if url is valid
 const isValidUrl = (url) => {
   return possibleDomains.some((domain) => url.includes(domain));
@@ -19,70 +41,64 @@ const isValidUrl = (url) => {
 
 //   extract channel id from url
 const getChannelId = (url) => {
-  if (isValidUrl(url)) {
-    if (url.includes("channel")) {
-      return url.split("/")[4];
-    } else if (url.includes("user")) {
-      return url.split("/")[4];
-    }
-    if (url.includes("c")) {
-      return url.split("/")[4];
-    } else {
-      return null;
+  let isValidChannelLink = possibleChannelLinks.some((domain) => url.includes(domain));
+  if (isValidChannelLink) {
+    switch(true) {
+      case url.includes("youtube.com/channel/"):
+        return url.split("/")[4];
+      case url.includes("youtube.com/user/"):
+        return url.split("/")[4];
+      case url.includes("youtube.com/c/"):
+        return url.split("/")[4];
+      case url.includes("youtube.com/@"):
+        return url.split("youtube.com/")[1];
+      default:
+        return null;
     }
   } else {
     return null;
   }
 };
+
 
 // verify is video valid
 const isVideo = (url) => {
   if (isValidUrl(url)) {
-    if (url.includes("youtu.be")) {
-      return true;
-    } else if (
-      url.includes("https://www.youtube.com/shorts/") ||
-      url.includes("https://youtube.com/shorts/") ||
-      url.includes("http://m.youtube.com/shorts/") ||
-      url.includes("https://m.youtube.com/shorts/")
-    ) {
-      return true;
-    } else if (url.includes("youtube.com/watch?v=")) {
-      return true;
-    } else if (url.includes("m.youtube.com/watch?v=")) {
-      return true;
-    } else {
-      return false;
-    }
+    return possibleVideoLinks.some((videoUrl) => url.includes(videoUrl));
   } else {
     return false;
   }
 };
+
 // extract video id from url
 const getVideoId = (url) => {
   if (isVideo(url)) {
-    if (url.includes("youtu.be")) {
-      return url.split("/")[3];
-    } else if (
-      url.includes("https://www.youtube.com/shorts/") ||
-      url.includes("https://youtube.com/shorts/")
-    ) {
-      return url.split("/")[4].split("?")[0];
-    } else if (url.includes("youtube.com")) {
-      return url.split("=")[1];
-    } else if (url.includes("m.youtube.com")) {
-      return url.split("=")[1];
-    } else {
-      return null;
+    
+    switch (true) {
+      case url.includes("youtu.be"):
+        return url.split("/")[3];
+      case url.includes("https://www.youtube.com/shorts/"):
+        console.log(url)
+        return url.split("/")[4].split("?")[0];
+      case url.includes("https://www.youtube.com/embed/"):
+        return url.split(".com/embed/")[1].split("?")[0]
+        return url;
+      case url.includes("https://www.youtube.com/live/"):
+        return url.split(".com/live/")[1].split("?")[0]
+        return url;
+      case url.includes("https://www.youtube.com/watch/"):
+        return url.split(".com/watch/")[1].split("?")[0]
+        return url;
+      case url.includes("youtube.com") || url.includes("m.youtube.com"):
+        return url.split("=")[1];
+      default:
+        return null;
     }
   } else {
+    console.log("not valid");
     return null;
   }
 };
-
-// let channelUrl = "https://www.youtube.com/watch?v=fdbfjsb";
-
-// console.log(isVideo(channelUrl));
 
 module.exports = {
   getVideoId,
